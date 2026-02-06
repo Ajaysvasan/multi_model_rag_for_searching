fc7bb7e4d855a8993ae95010aeea7be0056f127a
Ajaysvasan <ajay192006@gmail.com>
2026-02-06 21:12:29 +0530

    feat: done the remaining methods in the cache layer

--- backend/cache_layer/cache.py
+++ backend/cache_layer/cache.py
@@ -1,14 +1,8 @@
-# Need to implement L1 cache , L2 cache , L3 cache
-# This is the cache layer pipeline
-# Logic to deside which topic reside on which layer and which topic to forget
-# and also a way to store the cache in way that the exsisting cache gets loaded automatically when the application is started
 """Cache module"""
 
 import os
 import sys
 from collections import OrderedDict
-
-from transformers.utils.type_validators import tensor_type_validator
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
@@ -54,60 +48,99 @@ class TopicCacheManager:
         self.l3_least_occurace_node = get_least_occuring_node(CACHE, level=3)
         self.l2_least_occurace_node = get_least_occuring_node(CACHE, level=2)
         self.l1_least_occurace_node = get_least_occuring_node(CACHE, level=1)
+        CACHE.move_to_end(self.l3_least_occurace_node)
+
+    def _get_key_(self, node: CacheNode) -> TopicKey:
+        target_key: TopicKey
+        for key, value in CACHE.items():
+            if value == node:
+                target_key = key
+        return target_key
 
     def _promote(self, topic_key: TopicKey, score: float):
-        # Case 1 -> the promotion layer has space to accomodate the new node
 
         node: CacheNode = CACHE[topic_key]
-
-        node.level += 1
+        # 3 -> 2 -> 1
+        node.level -= 1
         if score > Config.L2_THRESHOLD and self.l2_cache_count < Config.L2_CAPACITY:
             self.l2_cache_count += 1
             self.l3_cache_count -= 1
+            self.l2_least_occurace_node = node
             CACHE[topic_key] = node
             # TODO: Here we also update the DB so that on the next load the updaed cache is loaded
-            return
+            return 0
         if score > Config.L1_THRESHOLD and self.l1_cache_count < Config.L1_CAPACITY:
             self.l1_cache_count += 1
             self.l2_cache_count -= 1
             CACHE[topic_key] = node
+            self.l1_least_occurace_node = node
             # TODO: Here we also update the DB so that on the next load the updaed cache is loaded
-            return
-
-        # Case 2 -> The promotion layer has no space to accomodate the new node
+            return 0
 
         if score > Config.L2_THRESHOLD:
-            curr_level_least_occurance_node = None
-            for key, value in CACHE.items():
-                if value == self.l2_least_occurace_node:
-                    curr_level_least_occurance_node = key
-            CACHE[curr_level_least_occurance_node].level -= 1
+            curr_level_least_occurance_node = self._get_key_(
+                self.l2_least_occurace_node
+            )
+            CACHE[curr_level_least_occurance_node].level += 1
             self.l2_cache_count -= 1
             CACHE[topic_key] = node
 
             # TODO: Here we also update the DB so that on the next load the updaed cache is loaded
-            return
+            return 0
 
         if score > Config.L1_THRESHOLD:
-            curr_level_least_occurance_node = None
-            for key, value in CACHE.items():
-                if value == self.l1_least_occurace_node:
-                    curr_level_least_occurance_node = key
-            CACHE[curr_level_least_occurance_node].level -= 1
+            curr_level_least_occurance_node = self._get_key_(
+                self.l1_least_occurace_node
+            )
+            CACHE[curr_level_least_occurance_node].level += 1
             self.l1_cache_count -= 1
             CACHE[topic_key] = node
-            return
+            return 0
         # TODO: Here we also update the DB so that on the next load the updaed cache is loaded
 
     def _demote(self, node: CacheNode):
-        pass
+        # 3 <- 2 <- 1
+        curr_node_key = self._get_key_(node)
+        CACHE[curr_node_key].level += 1
+        if node.level == 1 and self.l2_cache_count < Config.L2_CAPACITY:
+            self.l1_cache_count -= 1
+            self.l2_cache_count += 1
+            return 0
+            # TODO: Here we also update the DB so that on the next load the updaed cache is loaded
+        if node.level == 2 and self.l3_cache_count < Config.L3_CAPACITY:
+            self.l2_cache_count -= 1
+            self.l3_cache_count += 1
+            return 0
+            # TODO: Here we also update the DB so that on the next load the updaed cache is loaded
 
-    def _remove_topic(self, node: CacheNode):
-        pass
+        if len(CACHE) == Config.L1_CAPACITY + Config.L2_CAPACITY + Config.L3_CAPACITY:
+            self._remove_topic()
+
+        if (
+            node.level == 1
+            and self.l2_cache_count == Config.L2_CAPACITY
+            and self.l3_cache_count < Config.L3_CAPACITY
+        ):
+            l2_least_occurace_node_key = self._get_key_(self.l2_least_occurace_node)
+            CACHE[l2_least_occurace_node_key].level += 1
+            self.l1_cache_count -= 1
+            self.l2_cache_count += 1
+            self.l3_cache_count += 1
+            return 0
+        if node.level == 2 and self.l3_cache_count < Config.L3_CAPACITY:
+            l2_least_occurace_node_key = self._get_key_(self.l2_least_occurace_node)
+            CACHE[l2_least_occurace_node_key].level += 1
+            self.l2_cache_count -= 1
+            self.l3_cache_count += 1
+            return 0
+
+    def _remove_topic(self):
+        CACHE.popitem()
 
     def lookup(self, key: TopicKey):
         # Return the state if the TopicKey is found in the Dict
         try:
+            # Checking if the topic of interest is in the current conversation window
             node: CacheNode = CACHE[key]
             node.state.access_count += 1
             if node.level != 0:
@@ -123,16 +156,19 @@ class TopicCacheManager:
                     and score < Config.L3_THRESHOLD
                     and self.l3_cache_count == Config.L3_CAPACITY
                 ):
-                    self._remove_topic(node)
+                    self._remove_topic()
+
+            CACHE[key] = node
             return node
         except:
-            # returns -1 indicating that the operation is not successful indicating that the node is not in any of the current cache
+            # The logic for conversation history checking goes here
+            # returns -1 indicating that the operation is not successful indicating that the node is not in any of the current cache after searching in the context history
             return -1
 
     def insert(self, key: TopicKey, state: TopicState):
         # here by default the new topic enters into the L3 cache
         if self.l3_cache_count == Config.L3_CAPACITY:
-            self._remove_topic(self.l3_least_occurace_node)
+            self._remove_topic()
             CACHE[key] = state
         else:
             CACHE[key] = state
